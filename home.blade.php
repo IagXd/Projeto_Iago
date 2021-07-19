fba998fdb1c34f2b92a7ba5ee5f13ada65855a30
YunkBakura <84637725+YunkBakura@users.noreply.github.com>
2021-07-19 17:56:33 -0300

    Add files via upload
    Site

---
+++ home.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.template')
+@section('template')
+ <!-- Masthead-->
+ <header class="masthead">
+    <div class="container position-relative">
+        <div class="row justify-content-center">
+            <div class="col-xl-10">
+                <div class="text-center text-Black " >
+                    <!-- Page heading-->
+                    <h1 class="mb-5">Lista com personagens negros dos animes</h1>
+                    <div class="container position-relative">
+                            <div class="row g-10">
+   <p class="mb-0 lead">Na mídia, frequentemente seus papéis são secundários ou apagados, parecendo mais cotas mesmo, mesmo nas mídias ocidentais. O que diríamos então de personagens negros em trabalhos do Japão, um país que por muitos anos se manteve isolado e por décadas teve seus rancores com o ocidente? Mas eles vêm mudando e aparecendo mais lá na terra do sol nascente. No começo apareciam esporadicamente, mas com o passar dos anos e tendo os animes atingido um status muito mais global, personagens das mais variadas etnias estão tendo mais espaço nas obras nipônicas. A seguir, confira uma pequena lista para exaltar personagens negros que estão lá..Apesar de terem pouca representatividade no universo dos animes, personagens negros quando aparecem desempenham papéis importantes em algumas franquias de animes mais populares.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Icons Grid-->
+
+<!-- Image Showcases-->
+<section class="showcase">
+    <div class="p-0 container-fluid">
+        <div class="row g-0">
+            <div class="text-white col-lg-6 order-lg-2 showcase-img" style="background-image: url('img/Bob Makihara (Tenjou Tenge).gif')"></div>
+            <div class="my-auto col-lg-6 order-lg-1 showcase-text">
+                <h2>Bob Makihara (Tenjou Tenge)   </h2>
+                <p class="mb-0 lead">Tenjou Tenge pode não ser o melhor exemplo de obra para com as personagens femininas da trama, mas Oh!Great teve um acerto em sua estreia como profissional regular ao criar Bob como um dos protagonistas. Bob é um sul africano que estuda no Japão, amigo do principal protagonista, Nagi. Apesar de aos poucos a trama começar a focar excessivamente em seu amigo, Bob continua tendo seu brilho. Seu estilo de luta é a capoeira, mas ele também é muito bom usando os punhos</p>
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="text-white col-lg-6 showcase-img" style="background-image: url('img/Tousen Kaname.gif')"></div>
+            <div class="my-auto col-lg-6 showcase-text">
+                <h2>Kaname Tousen - Bleach</h2>
+                <p class="mb-0 lead">Kaname Tousen é o ex-capitão da 9 Divisão do Gotei 13 que se aliou a Aizen. Apesar de ser cego, suas habilidades e seu poder são notáveis.Tōsen tem pele escura e tranças castanho-escuros. Ele tem olhos sem pupilas, que são uma lavanda pálido e é cego desde o nascimento. Enquanto um capitão que ele usava o uniforme tradicional Shinigami com as mangas arregaçadas até os ombros e haori de seu chefe sobre isso, o uniforme é amarrado com uma faixa obi roxo. Ele também usava um lenço laranja em volta do pescoço, botas brancas (em vez de as sandálias normais), e preto, luvas sem dedos. Ele normalmente usava óculos claros, embora isto não foi sempre o caso. Ele manteve seu cabelo amarrado para trás em um rabo de cavalo.
+
+                </p>
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="text-white col-lg-6 order-lg-2 showcase-img" style="background-image: url('img/Kilik-Rung-2.gif')"></div>
+            <div class="my-auto col-lg-6 order-lg-1 showcase-text">
+                <h2>Kilik Rung - Soul Eater</h2>
+                <p class="mb-0 lead">Atsushi Ohkubo criou o Kilik por reconhecer que poucos mangás contêm personagens africanos; Ele baseou Kilik no personagem do personagem central em Kirikou e a Feiticeira , um filme que desenha elementos do conto popular da África Ocidental e um filme de que ele gostou. [6] Seu sobrenome, "Rung", é uma tradução de "Rungu", em que o referido nome é derivado de "Mulungu", um nome comum para a divindade criadora africana. [2].</p>
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="text-white col-lg-6 showcase-img" style="background-image: url('img/Kanaria - Hunter X Hunter.gif')"></div>
+            <div class="my-auto col-lg-6 showcase-text">
+                <h2>Kanaria - Hunter X Hunter</h2>
+                <p class="mb-0 lead">Kanaria é uma mordomo da familia Zaoldyeck, que permanece entre os portões da residencia, onde não deixa ninguém passar, é bastante agil e forte, usando sua bengala para acertar qualquer pessoa que cruze os portões.
+
+                </p>
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="text-white col-lg-6 order-lg-2 showcase-img" style="background-image: url('img/Muhammad Avdol - Jojo Bizzare Adventure.gif')"></div>
+            <div class="my-auto col-lg-6 order-lg-1 showcase-text">
+                <h2>Muhammad Avdol - Jojo Bizzare Adventure </h2>
+                <p class="mb-0 lead">De nacionalidade egipcia, Muhammad é um grande amigo de Joseph Joestar e auxilia Jotaro Kujo em sua missão de confrontar Dio Brando. Possui seu stand Magician's Red que é capaz de criar chamas extremamente poderosas.</p>
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="text-white col-lg-6 showcase-img" style="background-image: url('img/Afro - Afro Samurai.gif')"></div>
+            <div class="my-auto col-lg-6 showcase-text">
+                <h2>Afro - Afro Samurai</h2>
+                <p class="mb-0 lead">É o protagonista da série Afro Samurai, teve destaque ao ser dublado pelo Samuel "Nick Furry" L. Jackson. Um espadachim incrivelmente talentoso, Afro teve o seu pai decapitado diante de si durante a infância. Afro tem por objetivo matar Justice, o homem que matou o seu pai para apoderar-se da bandana de número um. A prontidão de Afro em matar pessoas que entrem no seu caminho impressiona as pessoas, que o chamam de "monstro". O espadachim vingativo trilha um caminho solitário e tem como único companheiro Ninja Ninja. Este, por sua vez, é o oposto em extroversão de Afro, que tende a mandá-lo "calar-se" com frequência. O desejo de vingar essa morte fez Afro crescer como um assassino sangue-frio, derramando o sangue de incontáveis pessoas pelo caminho. É o detentor da banda de Número Dois. Pelo seu protagonismo na seríe dá o posto de 1º lugar.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Testimonials-->
+
+<!-- Call to Action-->
+<section class="text-center text-Black call-to-action" id="signup">
+    <div class="container position-relative">
+        <div class="row justify-content-center">
+            <div class="row g-0">
+                <div class="text-white col-lg-6 order-lg-2 showcase-img" style="background-image: url('img/iagu.jpg')"></div>
+                <div class="my-auto col-lg-6 order-lg-1 showcase-text">
+                    <h2>Iago dos Santos Gomes</h2>
+                    <p class="mb-0 lead">Iago  - Ter que escrever sobre racismo hoje em dia é uma coisa estranha, pois sempre aparece alguém para dizer que racismo não existe mais e invalidar as reivindicações sociais. Entretanto, basta uma pessoa reclamar da falta de diversidade em alguma série que aparecem várias outras falando “ah, mas vai assistir as séries x, y e z que lá tem gente negra”. Não irei discutir aqui se o que ele falou está certo ou não. O fato é que muitas pessoas ainda tentam invalidar a nossa luta. Bem, espero que o texto deixe vocês pensando um pouco sobre a retratação de personagens negros nos animes e o quão importante é falar sobre isso.</p>
+                </div>
+            </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="text-center text-Black " id="signup">
+
+        <div class="row justify-content-center">
+            <div class="row g-0">
+                <div class="text-white col-lg-6 showcase-img" style="background-image: url('img/Ekko.gif')"></div>
+                <div class="my-auto col-lg-6 showcase-text">
+                    <form action="mail.php" method="post" class="text-white card-header" style="background-color: #7688b9;">
+                        <div class="card border-primary rounded-0">
+                            <div class="text-white card-header" >
+                                <div class="text-white card-header" style="background-color: #7688b9; class="py-2 text-center text-white bg-info" >
+                                    <h3><i  class="fa fa-envelope" ></i> Fale Conosco</h3>
+                                </div>
+
+                            </div>
+                            <div class="p-3 card-body">
+
+                                <!--Body-->
+                                <div class="form-group" >
+                                    <div class="mb-2 input-group">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-user text-info"></i></div>
+                                        </div>
+                                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="mb-2 input-group">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-envelope text-info"></i></div>
+                                        </div>
+                                        <input type="email" class="form-control" id="nome" name="email" placeholder="xxxxx@gmail.com" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group"class="text-white card-header" style="background-color: #2b2c2c;">
+                                    <div class="mb-2 input-group">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text"><i class="fa fa-comment text-info"></i></div>
+                                        </div>
+                                        <textarea class="form-control" placeholder="Digite sua mensagem" required></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="text-center"class="text-white card-header" style="background-color: #2b2c2c;">
+                                    <input type="submit" value="Enviar" class="py-2 btn btn-info btn-block rounded-0"class="text-white card-header" style="background-color: #7688b9;">
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
